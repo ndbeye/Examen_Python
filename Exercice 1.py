3e93491e35bc3aa801118ee98eef75aacbfaac8f
BEYE_Ndiaga <158743024+ndbeye@users.noreply.github.com>
2024-02-10 13:06:49 +0000

    Update Exercice 1.py

--- Exercice 1.py
+++ Exercice 1.py
@@ -18,7 +18,7 @@ plt.fill_between(x_courbe, y_courbe, color='blue', alpha=0.3, where=(y_courbe > 
 plt.fill_between(x_courbe, y_courbe, color='green', alpha=0.3, where=(y_courbe <= y_square))
 plt.xlabel('x')
 plt.ylabel('y')
-plt.title('Courbe $x^5$ et Carré')
+plt.title(' Courbe d’équation x⁵ sur l’intervalle [0;1]')
 plt.grid(True)
 plt.legend()
 plt.axis('equal')
